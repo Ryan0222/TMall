d82dd89ad398e5b6d4f0bfc6fd28ca53f5212b04
Ruizhe Xie <xierz96@hotmail.com>
2021-04-27 23:34:40 -0700

    update sql

---
+++ SQL/Productimage.sql
@@ -0,0 +1,7 @@
+CREATE TABLE productimage (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  pid int(11) DEFAULT NULL,
+  type varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_productimage_product FOREIGN KEY (pid) REFERENCES product (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
